137683e7c0b4f307abb903df84de1b4d8ad29486
Coretta Dawani <bsc-com-14-18@cc.ac.mw>
2020-10-20 13:31:14 +0200

    Initial commit

---
+++ School.java
@@ -0,0 +1,15 @@
+public class Student {
+    Private String name;
+    Private double [] grade = new double[];
+    Private int totNumberofStudents;
+}
+Student(){
+}
+        Student(String name, double[] grades, int totNumberofStudents){
+            this.name;
+            this.grade;
+            this.totNumberofStudents;
+            }
+public void setName(String name){
+    
+}
